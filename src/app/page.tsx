1bb71351707517032a0305d29e49c1da92b3eeb0
kashish567 <kashishbhanushali7@gmail.com>
2024-09-03 14:32:21 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,35 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import pointsData from "../../public/didyouknow.json";
 
 const Home = () => {
-  const points = [
-    "Figma enables collaboration for seamless teamwork.",
-    "Accessible on any operating system via the browser.",
-    "Simplifies complex shape creation and editing.",
-    "Automates responsive design elements arrangement.",
-    "Offers interactive prototyping capabilities within the platform.",
-    "Boasts a rich community and extensive plugin library.",
-  ];
+  const getRandomPoints = (category: any) => {
+    const categoryPoints = [...pointsData[category]]; // Copying the array
+    const numberOfPoints = Math.floor(Math.random() * (7 - 5 + 1)) + 5; // Random number between 5 and 7
+    const shuffledPoints = categoryPoints.sort(() => 0.5 - Math.random());
+    return shuffledPoints.slice(0, numberOfPoints);
+  };
+
+  const [selectedPoints, setSelectedPoints] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSelectedPoints(getRandomPoints("design")); // Change category as needed
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 sm:p-12 md:p-16 lg:p-24 bg-[#dfe1e8]">
-      <div className="bg-transparent rounded-3xl border-2 flex flex-col items-center justify-center border-black p-4 w-full max-w-[90%] sm:max-w-[40rem] relative sm:mt-0">
-        <div className="bg-[#eb8f80] rounded-2xl p-3 px-6 sm:px-10 mb-4 text-center absolute top-0 -translate-y-1/2 -rotate-6 text-black">
-          <h2 className="text-lg sm:text-xl md:text-2xl font-semibold">
+      <div className="bg-transparent rounded-lg border-2 flex flex-col items-center justify-center border-black p-4 w-full max-w-[80%] sm:max-w-[30rem] h-[24rem] relative sm:mt-0">
+        <div className="bg-[#eb8f80] rounded-2xl p-3 px-6 sm:px-8 absolute top-0 -translate-y-1/4 -rotate-6 text-black">
+          <h2 className="text-sm sm:text-lg md:text-xl font-semibold">
             Did You Know?
           </h2>
         </div>
-        <ul className="mt-14 list-disc list-inside space-y-1 mb-10">
-          {points.map((point, index) => (
+        <ul className="mt-4 list-disc list-inside space-y-1">
+          {selectedPoints.map((point, index) => (
             <li
               key={index}
-              className="flex items-start text-sm sm:text-lg text-black w-full before:content-['•'] before:text-black before:mr-2"
+              className="flex items-start mt-5 text-xs sm:text-sm text-black w-full before:content-['•'] before:text-black before:mr-2"
             >
               {point}
             </li>
