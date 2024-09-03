1678636e91fd08a7c6652d117668b81e4d2f5136
kashish567 <kashishbhanushali7@gmail.com>
2024-09-03 14:55:42 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,8 @@ const Home = () => {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6 sm:p-10 md:p-12 lg:p-20 bg-[#dfe1e8]">
-      <div className="bg-transparent rounded-lg border-2 flex flex-col items-center justify-center border-black p-4 w-full max-w-[80%] sm:max-w-[26rem] h-[22rem] relative sm:mt-0">
+    <main className="flex h-screen flex-col items-center justify-center p-6 sm:p-12 md:p-16 lg:p-24 bg-[#dfe1e8]">
+      <div className="bg-transparent rounded-lg border-2 flex flex-col items-center justify-center border-black p-4 max-w-[80%] sm:max-w-[30rem] h-[27rem] relative sm:mt-0">
         <div className="bg-[#eb8f80] rounded-2xl p-3 px-6 sm:px-8 absolute top-0 -translate-y-1/4 -rotate-6 text-black">
           <h2 className="text-sm sm:text-lg md:text-xl font-semibold">
             Did You Know?
@@ -29,7 +29,7 @@ const Home = () => {
           {selectedPoints.map((point, index) => (
             <li
               key={index}
-              className="flex items-start mt-5 text-xs sm:text-sm text-black w-full before:content-['•'] before:text-black before:mr-2"
+              className="flex items-start text-xs sm:text-sm text-black w-full before:content-['•'] before:text-black before:mr-2"
             >
               {point}
             </li>
